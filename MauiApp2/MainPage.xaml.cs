bb7e55b12705f2cd31341ec0fbdf8155fd6c648b
yulia <yuliia_yaresko@knu.ua>
2024-11-16 14:10:06 +0200

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Maui.Controls;
+namespace MauiApp2;
+
+
+    public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Message", "Hello World!", "OK");
+    }
+}
